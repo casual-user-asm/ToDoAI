c4dd9755b48443d102eee97b8e178fbf36edc012
rage <casual-user@tutanota.com>
2024-08-01 03:27:49 +1245

    Manage Tasks Section(only styles)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
 import Home from './pages/Home'
 
 function App() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,68 @@
+import { Col, Row, Divider, Button, Input, Avatar, List, Space } from 'antd'
+import styles from './Home.module.css'
+import { useEffect, useState } from 'react'
+import { CloseCircleFilled, CheckSquareOutlined } from '@ant-design/icons'
+
 export default function Home() {
+    const data = [
+        {
+            title: 'Ant Design Title 1',
+        },
+        {
+            title: 'Ant Design Title 2',
+        },
+        {
+            title: 'Ant Design Title 3',
+        },
+        {
+            title: 'Ant Design Title 4',
+        },
+    ]
+
     return (
-        <div className="container text-center">
-            <div className="row">
-                <div className="col-sm-3">Level 1: .col-sm-3</div>
-                <div className="col-sm-9">
-                    <div className="row">
-                        <div className="col-8 col-sm-6">
-                            Level 2: .col-8 .col-sm-6
-                        </div>
-                        <div className="col-4 col-sm-6">
-                            Level 2: .col-4 .col-sm-6
-                        </div>
+        <div className={`${styles.wrapper}`}>
+            <Row className={`${styles.container}`}>
+                <Col className={`${styles.content}`} flex={2}>
+                    <Divider orientation="left">
+                        <p className={`${styles.text}`}>Manage your tasks</p>
+                    </Divider>
+                    <Space.Compact
+                        style={{
+                            width: '85%',
+                            'margin-left': '35px',
+                        }}
+                    >
+                        <Input placeholder="Write your next task" />
+                        <Button className={`${styles.button}`} type="primary">
+                            Add task
+                        </Button>
+                    </Space.Compact>
+                    <div className={`${styles.listWrapper}`}>
+                        <List
+                            itemLayout="horizontal"
+                            dataSource={data}
+                            renderItem={(item, index) => (
+                                <List.Item>
+                                    <p className={`${styles.textTask}`}>
+                                        {item.title}
+                                    </p>
+                                    <CheckSquareOutlined
+                                        className={`${styles.completeBut}`}
+                                        title="Complete task"
+                                    />
+                                    <CloseCircleFilled
+                                        className={`${styles.deleteBut}`}
+                                        title="Delete task"
+                                    />
+                                </List.Item>
+                            )}
+                        />
                     </div>
-                </div>
-            </div>
+                </Col>
+                <Col className={`${styles.content2}`} flex={3}>
+                    3 / 5
+                </Col>
+            </Row>
         </div>
     )
 }
